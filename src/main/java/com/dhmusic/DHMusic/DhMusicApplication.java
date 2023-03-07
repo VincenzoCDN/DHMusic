197aeed942d337274ae3d2cd0fed663a92bbf6a7
Vincenzo CDN <vincenzocdn@gmail.com>
2023-03-07 15:52:53 +0100

    Initial commit

---
+++ src/main/java/com/dhmusic/DHMusic/DhMusicApplication.java
@@ -0,0 +1,13 @@
+package com.dhmusic.DHMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhmusic/DHMusic/DhMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhmusic.DHMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
