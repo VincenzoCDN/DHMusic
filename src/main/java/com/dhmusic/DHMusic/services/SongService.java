d8bd735719d760ebd8021ca9a02fef57fdc9ecfd
Stella Romeo <stellaromeo21@gmail.com>
2023-03-16 16:36:26 +0100

    add entities modificated Creation Account Controller Created repository to find songs by title, album, genre Add methods to add Song Add set

--- src/main/java/com/dhmusic/DHMusic/services/SongService.java
+++ src/main/java/com/dhmusic/DHMusic/services/SongService.java
@@ -1,13 +1,25 @@
 package com.dhmusic.DHMusic.services;
 
+import com.dhmusic.DHMusic.entities.account.entities.Artist;
 import com.dhmusic.DHMusic.entities.content.entities.Song;
-import org.springframework.stereotype.Service;
+import com.dhmusic.DHMusic.repositories.content.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 //@Service
 public class SongService {
 
-    public Song addSong(String title, String artist, String genre, float duration){
+    @Autowired
+    private SongRepository songRepository; // per accesso al database
+
+    public SongService(SongRepository songRepository){
+        this.songRepository = songRepository;
+    }
+
+    public Song addSong(String title, Artist artist, String genre, float duration){ // discutere genere
         Song newSong = new Song();
+        newSong.setTitle(title);
+        newSong.setArtistOfSong(artist);
+        newSong.setLength(duration);
         return newSong;
     }
 
