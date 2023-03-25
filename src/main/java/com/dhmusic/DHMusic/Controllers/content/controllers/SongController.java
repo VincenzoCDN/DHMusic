c2a4788ccca400f65f9e571b08342ea372815f08
Stella Romeo <stellaromeo21@gmail.com>
2023-03-25 11:34:23 +0100

    add entities modificated Creation Account Controller Created repository to find songs by title, album, genre Add methods to add Song Add set Modified method and inserted an exception if the registration fields such as title and artist are not inserted Modified Song's Controller to add song. Add method to delete song to @Service Add controller to delete song Processing method to update edited method update, fixed error Add message to error in methods to update song and i try to put exception in addSong Add boolean to see if song is public or not. Add input to method addSong and try-catch-finally to close Scanner added the condition, if the song is public and the user is an administrator, he can delete the song Added another conditions to method to delete a song Added GetMapping to view all songs or to view songs by id Added exception into method deleteSong. Delete Scanner and add @PutMapping and method to update into songController Add method to search songs prova song Modificated controller and service Modificated in "create" Add CRUD (Create, Read, Delete) Song code cleaning

--- src/main/java/com/dhmusic/DHMusic/Controllers/content/controllers/SongController.java
+++ src/main/java/com/dhmusic/DHMusic/Controllers/content/controllers/SongController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/songs")
@@ -53,10 +52,7 @@ public class SongController {
     }
     //---------------------------------------------------------------------------------------
     //Aggiorna un Song nel database
-    @PutMapping("/update/{id}")                                     //TODO perchè non funziona
-    public Song updateSong (@PathVariable Long id, @RequestBody Song song) {
-        return songService.updateSong(song);
-    }
+
 
 
 }
--- src/main/java/com/dhmusic/DHMusic/services/SongService.java
+++ src/main/java/com/dhmusic/DHMusic/services/SongService.java
@@ -1,18 +1,10 @@
 package com.dhmusic.DHMusic.services;
 
-import com.dhmusic.DHMusic.entities.account.entities.Artist;
-import com.dhmusic.DHMusic.entities.account.entities.User;
-import com.dhmusic.DHMusic.entities.content.entities.Album;
 import com.dhmusic.DHMusic.entities.content.entities.Song;
 import com.dhmusic.DHMusic.repositories.content.repositories.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class SongService {
@@ -23,42 +15,8 @@ public class SongService {
 
     //---------------------------------------------------------------------------------------
     public Song addSong(Song song) {
-
-        /*Song existSong = songRepository.findSongByTitle(song.getTitle()).get(1);
-        //  && songRepository.findSongByArtist(song.getArtistOfSong());
-        if (existSong != null) {
-            throw new RuntimeException("Song exist!");
-        }
-        if (song.getTitle() == null || song.getArtistOfSong() == null) {
-            throw new IllegalArgumentException("Mistake! Required fields are missing");
-        }
-*/
         return songRepository.save(song);
-
     }
-    //---------------------------------------------------------------------------------------
-    /*public void deleteSong(@RequestBody Song song) {
-        User user = new User();
-        try {
-            Song existSong = songRepository.findSongByTitle(song.getTitle()); //mettere indice?
-            if (existSong == null) {
-                throw new RuntimeException("Song not exist!");
-            }
-            if (existSong.isPublic() && user.isAdmin1()
-                    || !existSong.isPublic() && !user.isAdmin1()
-                    || !existSong.isPublic() && user.isAdmin1()) { // da vedere
-                songRepository.delete(song);
-            } else {
-                throw new RuntimeException("You don't have permission to delete this song");
-            }
-        } catch (Exception e) {
-            System.out.println("An error has occurred:" + e.getMessage());
-        }
-    }
-
-     */
-
-
     //---------------------------------------------------------------------------------------
     public Song updateSong(Song song) {
         Song existSong = songRepository.findSongById(song.getId()); // id string o long?
@@ -74,33 +32,7 @@ public class SongService {
         return songRepository.save(existSong);
     }
     //---------------------------------------------------------------------------------------
-    public Song searchSong(Song song) {
-        String title = song.getTitle();
-        Album album = song.getAlbumOfSong();
-        Artist artist = song.getArtistOfSong();
-        Long id = song.getId();
-        String genre = song.getGenre();
-        /*if (title == null && artist == null) {  //da ragionare
-            throw new IllegalArgumentException("At least one search parameter is required");
-        }
-        if (title != null) {
-            return songRepository.findSongByTitle(title);
-        }
-        if (album != null) {
-            return songRepository.findSongByAlbumOfSong(album);
-        }
-//        if (artist != null) {
-//            return songRepository.findSongByArtist(artist);
-//        }
-        if (id != null) {
-            return songRepository.findSongById(id);
-            }
-       */
 
-        return songRepository.findSongByTitle(title); // da vedere
-
-    }
-    //-----------------------------------------------------------------------
     public void deleteSong(Long id) {                              //OK
          Song existSong = songRepository.findSongById(id);
          if(existSong != null) {
