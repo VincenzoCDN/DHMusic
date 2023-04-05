e0ceb3fcba2bb069800c7594e926e86ee51bf7bf
Giuseppe Albanese <giuse00876alba@hotmail.com>
2023-04-05 18:34:38 +0200

    Value messa

--- src/main/java/com/dhmusic/DHMusic/email/EmailService.java
+++ src/main/java/com/dhmusic/DHMusic/email/EmailService.java
@@ -3,6 +3,7 @@ package com.dhmusic.DHMusic.email;
 import com.dhmusic.DHMusic.entities.account.entities.User;
 import com.dhmusic.DHMusic.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,14 @@ public class EmailService {
  //   @Autowired
    // private UserService userService;
 
+    @Value("${spring.mail.username}")
+    String email1;
+
     public void sendTo(String email, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject(title);
-        message.setFrom("dhmusicstreamingsong@gmail.com");
+        message.setFrom(email1);
         message.setText(text);
         javaMailSender.send(message);
     }
