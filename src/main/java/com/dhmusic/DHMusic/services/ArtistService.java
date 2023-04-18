6e9134ce5a9b4b6055d772bede93cf81debd12f5
Giuseppe Albanese <giuse00876alba@hotmail.com>
2023-04-18 17:33:11 +0200

    fix creation Artist in service  delete e update

--- src/main/java/com/dhmusic/DHMusic/services/ArtistService.java
+++ src/main/java/com/dhmusic/DHMusic/services/ArtistService.java
@@ -75,6 +75,7 @@ public class ArtistService {
             throw new Exception("Exist artist Name");
 
         } else if (existUserId(artistDTO)==false) {
+            logger.error("userId does not exist "+ artistDTO.getUserId());
             throw new Exception("User id does not exist");
 
         } else if (artistRepository.findByUserId(artistDTO.getUserId()) != null) {
@@ -92,6 +93,10 @@ public class ArtistService {
             logger.warn("the artist to be modified does not exist");
             throw new Exception("the artist does not exist");
 
+        }
+        if(existArtistName(artistEditDTO) == true){
+            logger.warn("there is already an artist with this name " +artistEditDTO.getArtistName());
+            throw new Exception("there is already an artist with this name ");
         }
         if (artistEditDTO.getArtistName() == null && artistEditDTO.getBio() == null && artistEditDTO.getUserId() == null ) {
 
