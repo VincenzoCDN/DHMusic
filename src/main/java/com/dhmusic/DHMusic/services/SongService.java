dd9d827b5ccccf82ec814e88553daa23bcd4e1a8
Stella Romeo <stellaromeo21@gmail.com>
2023-04-19 10:58:27 +0200

    Create method to update song. Delete set album and artist. Add try/catcg , delete old methods, add ResponseEntity status. Correction Song Mapper. Add creation date (into platform) Add exception if not exist the artist Update Date to Song Add exception to create song if song's title exist. Create Get by Id with SongDTO and using JsonFormat to Date Inserted Logger in SongService and inserted all the info inside each method

--- src/main/java/com/dhmusic/DHMusic/services/SongService.java
+++ src/main/java/com/dhmusic/DHMusic/services/SongService.java
@@ -8,6 +8,8 @@ import com.dhmusic.DHMusic.mapper.SongMapper;
 import com.dhmusic.DHMusic.repositories.account_repositories.ArtistRepository;
 import com.dhmusic.DHMusic.repositories.content.repositories.AlbumRepository;
 import com.dhmusic.DHMusic.repositories.content.repositories.SongRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SongService {
+
+    Logger logger = LoggerFactory.getLogger(SongService.class);
 
     @Autowired
     private SongMapper songMapper;
@@ -32,32 +36,38 @@ public class SongService {
     public void deleteSong(Long id) throws Exception {
          Song existSong = songRepository.findSongById(id);
          if(existSong != null) {
+             logger.info("The song %d has been deleted", id);
              songRepository.delete(existSong);
          }else{
+             logger.error("The song %d does not exist!", id);
              throw new RuntimeException("Song not exist!");
          }
     }
     //-----------------------------------------------------------------------
     public void deleteAll() {
-        //insert conditions       TODO
+        logger.info("All songs have been deleted.");
       songRepository.deleteAll();
         }
     //-----------------------------------------------------------------------
     public ResponseEntity<Song> addSong(SongDTO song) throws Exception {
         Song existSong = songRepository.findSongById(song.getId());
         if (existSong != null) {
+            logger.error("The song you were looking for does not exist!");
             throw new RuntimeException("Song exist!");
         }
         if (song.getTitle() == null || song.getIdArtistOfSong() == null) {
-            throw new IllegalArgumentException("Mistake! Required fields are missing");
+            logger.error("No requirements have been added");
+            throw new IllegalArgumentException("Mistake! Required fields are missing.");
         }
         if (songRepository.findSongByTitle(song.getTitle()) != null){
+            logger.error("The user entered an existing title.");
             throw new Exception("The title exist!");
         }
         if (artistRepository.findArtistById(song.getIdArtistOfSong()) == null){
+            logger.error("The user has not entered an existing artist.");
             throw new Exception("Artist not exist!");
         }
-
+        logger.info("The user has entered a new song.");
         return ResponseEntity.status(HttpStatus.CREATED).body(songRepository.save(songMapper.toSong(song)));
     }
 
@@ -67,8 +77,10 @@ public class SongService {
        public ResponseEntity<?> updateSong(Long id,SongDTO updateSong) {
             Song existSong = songRepository.findSongById(id); // id string o long?
             if (existSong == null) {
-               return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Song not found");
+                logger.error("The song %d was not found", id);
+               return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Song %d not found");
             } else if (!existSong.getId().equals(updateSong.getId())) {
+                logger.error("Song %d does not match", id);
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Song id does not match");
             } else {
                 existSong.setTitle(updateSong.getTitle());
@@ -81,6 +93,7 @@ public class SongService {
                 songRepository.save(existSong);
 
             }
+            logger.info("The song %d has been successfully updated", id);
             return ResponseEntity.ok(existSong);
         }
 
